f6061d4fffbde6c529fde35a9dbbcdb64d90cc13
Meidhy <meidhy@klinikpintar.id>
2022-04-03 21:23:59 +0700

    initial commit

---
+++ auth/src/models/User.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id
+      delete ret.password;
+      delete ret.__v;
+    }
+  }
+});
+UserSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+UserSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+const User = mongoose.model<UserDoc, UserModel>('User', UserSchema);
+
+
+export { User };
