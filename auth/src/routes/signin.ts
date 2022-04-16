5592b4c14bb6f733bc91dbeef106a0971d3757ed
Meidhy <meidhy@klinikpintar.id>
2022-04-16 16:18:23 +0700

    feat: finish sign in route and split error validation handler

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,46 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-request'
+import { User } from "../models/User";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  //
-  res.send(req.body);
+router.post('/api/users/signin', [
+body('email')
+.isEmail()
+.withMessage('Email must be valid'),
+body('password')
+.trim().notEmpty().withMessage('You must supply a password')
+],
+validateRequest,
+async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email })
+  if(!existingUser) {
+    throw new BadRequestError('Bad credentials!');
+  }
+
+  const passwordMatch = await Password.compare(existingUser.password, password)
+
+  if(!passwordMatch) {
+    throw new BadRequestError('Bad credentials!');
+  }
+
+ // Generate JWT
+ const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.JWT_KEY!);
+
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(200).send(existingUser);
 });
 
 export { router as signInRouter };
